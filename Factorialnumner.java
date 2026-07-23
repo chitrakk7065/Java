41225e1b81c6e9354fc5067fe302b4b572c603b8
Chitra K K <chitrakk7065@gmail.com>
2026-07-23 22:38:15 +0530

    Add FactorialList class to compute factorials
    Implement a program to calculate and print the factorial of numbers from 1 to 10.

---
+++ Factorialnumner.java
@@ -0,0 +1,10 @@
+class FactorialList {
+  public static void main(String[] args){
+    for(int i=1;i<=10;i++){
+      int factorial=1;
+      for(int j=1;j<=i;j++){
+        factorial*=j;
+        System.out.println("Factorial of "+i+"is"+factorial);
+      }
+    }
+  }
